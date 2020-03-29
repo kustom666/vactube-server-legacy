25a66d897b8bad8475771264d93dd3b97180b405
Paul Forti <paulforti@quoil.app>
2020-03-29 18:36:53 +1100

    add date and time to logs

--- src/server.cc
+++ src/server.cc
@@ -1,4 +1,5 @@
 #include <mutex>
+#include <iomanip>
 #include <iostream>
 #include <grpc/grpc.h>
 #include <grpcpp/server.h>
@@ -12,8 +13,10 @@ class VactubeImpl final : public quoil::Vactube::Service {
   grpc::Status TextChat(grpc::ServerContext* context, grpc::ServerReaderWriter<quoil::TextMessage, quoil::TextMessage>* stream) override {
     quoil::TextMessage message;
     while(stream->Read(&message)) {
+      auto t = std::time(nullptr);
+      auto tm = *std::localtime(&t);
       std::unique_lock<std::mutex> lock(mu_);
-      std::cout << message.username() << ": " << message.messagetext() << std::endl;
+      std::cout << std::put_time(&tm, "[%d-%m-%Y %H-%M-%S] ") << message.username() << ": " << message.messagetext() << std::endl;
       history_.push_back(message);
     }
     return grpc::Status::OK;
